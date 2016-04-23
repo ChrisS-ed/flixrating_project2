9de763f8c07ebbf55ec23f584bb9ebc3481c12f7
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-04-23 12:00:37 +0100

    getting 200 code back from API

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
+var http = require('http');
 
-app.get('/', function(req, res) {
+app.get('/films', function(req, res) {
   res.send('Hello World!') 
 });
 
 app.listen('3000', function() {
   console.log('Serving on port 3000')
+});
+
+app.get('/films/:filmTitle', function(request, response) {
+    // In here we will make our HTTP request to the OMDB API
+
+    http.get('http://www.omdbapi.com/?t=FILMTITLE'+ request.params.filmTitle + '&y=&plot=short&r=json', function(res) {
+        console.log(res.statusCode);
+
+    });
+
 });
