47b97247d2fd0a567dc97ad4b5caa6bdd010979a
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-04-26 14:33:26 +0100

    commented out local storage code

--- public/js/client.js
+++ public/js/client.js
@@ -4,7 +4,6 @@ var Film = function(filmTitle) {
 }
 
 Film.prototype = {
-
   get: function( callback ) {
     var that = this;
     var request = new XMLHttpRequest();
@@ -22,20 +21,19 @@ window.onload = function() {
   var form = document.querySelector('#filmSearch');
   var input = document.querySelector('#filmInput');
   var filmView = document.querySelector('#filmDisplay');
-  var storedFilmsView = document.querySelector('#storedFilms');
+  // var storedFilmsView = document.querySelector('#storedFilms');
 
-  var films = JSON.parse(localStorage.getItem('films')) || [];
-  //var films = [];
+  // var films = JSON.parse(localStorage.getItem('films')) || [];
 
-  var displayFilms = function() {
-    storedFilmsView.innerHTML = '';
-    for (film in films) {
-      var data = films[film];
-      var li = document.createElement('li');
-      li.innerHTML = "<h4>" + data.Title + "</h4>";
-      storedFilmsView.appendChild(li);
-    }
-  }
+  // var displayFilms = function() {
+  //   storedFilmsView.innerHTML = '';
+  //   for (film in films) {
+  //     var data = films[film];
+  //     var li = document.createElement('li');
+  //     li.innerHTML = "<h4>" + data.Title + "</h4>";
+  //     storedFilmsView.appendChild(li);
+  //   }
+  // }
 
   form.onsubmit = function(event) {
     event.preventDefault();
@@ -49,12 +47,12 @@ window.onload = function() {
       var filmDisplay = "<h4>" + data.Title + "</h4>";
       filmView.innerHTML = filmDisplay;
 
-      // document.querySelector('#addBook').onclick = function() {
-         films.push(data);
-         localStorage.setItem('films', JSON.stringify(films));
-         console.log("From local storage: ", JSON.parse(localStorage.getItem('films'))[0].Title);
-        displayFilms();
-      // }
+      // add film to films array and put into local storage
+        //  films.push(data);
+        //  localStorage.setItem('films', JSON.stringify(films));
+        //  console.log("From local storage: ", JSON.parse(localStorage.getItem('films'))[0].Title);
+        // displayFilms();
+      
     })
 
   }
